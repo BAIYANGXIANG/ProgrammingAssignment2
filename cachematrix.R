a85c8f67c3f1646083199150192a6d3758788076
BAIYANGXIANG <bxiang2@ncsu.edu>
2014-12-11 21:18:00 -0500

    write the new R code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the two function are working together
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        iv <- NULL
+        set <- function(y) {
+                x <<- y
+                iv <<- NULL
+        }
+        ## below functions are not ran but as tributes of the object
+        ## return the original x
+        get <- function() {x}
+        ## store the inversion
+        setiv <- function(inversion) {iv <<- inversion}
+        ## show the inversion 
+        getiv <- function() {iv}
+        list(set = set, get = get,
+             setiv = setiv,
+             getiv = getiv)
+      
+             
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve 
+## should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## x is an object
+        iv <- x$getiv()
+        if(!is.null(iv)) {
+                message("getting cached data")
+                return(iv)
+        }
+        data <- x$get()
+        # this is call the tribute of the x object created above
+        iv <- solve(data, ...)
+        x$setiv(iv)
+        iv
 }
